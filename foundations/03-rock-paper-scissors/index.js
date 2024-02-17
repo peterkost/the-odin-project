834b5772eb9604f9ae7a0683837fa14fa0992828
peterkost <46546031+peterkost@users.noreply.github.com>
2024-02-17 13:34:38 -0500

    Add UI to rock paper scissors

--- foundations/03-rock-paper-scissors/index.js
+++ foundations/03-rock-paper-scissors/index.js
@@ -1,25 +1,59 @@
-let moves = ['rock', 'paper', 'scissors']
+document.querySelector('#rock').addEventListener('click', () => playRound('rock'))
+document.querySelector('#paper').addEventListener('click', () => playRound('paper'))
+document.querySelector('#scissors').addEventListener('click', () => playRound('scissors'))
 
+let moves = ['rock', 'paper', 'scissors']
 let getComputerChoice = () => moves[Math.floor(Math.random() * moves.length)]
 
-let playRound = (playerSelection, computerSelection) => {
-    p = playerSelection.toLowerCase()
-
+let getRoundResult = (pS, computerSelection) => {
     switch (computerSelection) {
         case 'rock':
-            return p === 'rock' ? 'Tie!' : p === 'paper' ? 'Player win!' : 'Computer win!'
+            return pS === 'rock' ? 't' : pS === 'paper' ? 'p' : 'c'
         case 'paper':
-            return p === 'paper' ? 'Tie!' : p === 'scissors' ? 'Player win!' : 'Computer win!'
+            return pS === 'paper' ? 't' : pS === 'scissors' ? 'p' : 'c'
         case 'scissors':
-            return p === 'scissors' ? 'Tie!' : p === 'rock' ? 'Player win!' : 'Computer win!'
+            return pS === 'scissors' ? 't' : pS === 'rock' ? 'p' : 'c'
     }
 }
 
-let playGame = () => {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What's your move?")
-        console.log(playRound(playerSelection, getComputerChoice()))
+const elComputerLastMove = document.querySelector('#computer-move')
+const elPlayerLastMove = document.querySelector('#player-move')
+const elResult = document.querySelector('#result')
+
+const elComputerScore = document.querySelector('#computer-score')
+const elPlayerScore = document.querySelector('#player-score')
+const elTieScore = document.querySelector('#tie-score')
+
+let computerScore, playerScore, tieScore
+computerScore = playerScore = tieScore = 0
+
+let playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice()
+    const res = getRoundResult(playerSelection, computerSelection)
+
+    let resString
+    if (res === 't') {
+        resString = 'Tie!'
+        elTieScore.textContent = ++tieScore
+    } else if (res === 'p') {
+        resString = 'Player victory!'
+        elPlayerScore.textContent = ++playerScore
+    } else {
+        resString = 'Computer victory!'
+        elComputerScore.textContent = ++computerScore
+    }
+
+    elComputerLastMove.textContent = computerSelection
+    elPlayerLastMove.textContent = playerSelection
+    elResult.textContent = resString
+
+
+    if (playerScore === 5) {
+        alert(`The player has won with a score of 5-${computerScore}!`)
+        location.reload()
+    } else if (computerScore === 5) {
+        alert(`The computer has won with a score of 5-${playerScore}.`)
+        location.reload()
     }
 }
 
-playGame()
