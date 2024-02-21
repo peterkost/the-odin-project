378e6c6c15875686df13cf826def00d123b242d3
peterkost <46546031+peterkost@users.noreply.github.com>
2024-02-21 16:32:07 -0500

    Format files

--- foundations/03-rock-paper-scissors/index.js
+++ foundations/03-rock-paper-scissors/index.js
@@ -1,59 +1,63 @@
-document.querySelector('#rock').addEventListener('click', () => playRound('rock'))
-document.querySelector('#paper').addEventListener('click', () => playRound('paper'))
-document.querySelector('#scissors').addEventListener('click', () => playRound('scissors'))
+document
+  .querySelector("#rock")
+  .addEventListener("click", () => playRound("rock"));
+document
+  .querySelector("#paper")
+  .addEventListener("click", () => playRound("paper"));
+document
+  .querySelector("#scissors")
+  .addEventListener("click", () => playRound("scissors"));
 
-let moves = ['rock', 'paper', 'scissors']
-let getComputerChoice = () => moves[Math.floor(Math.random() * moves.length)]
+let moves = ["rock", "paper", "scissors"];
+let getComputerChoice = () => moves[Math.floor(Math.random() * moves.length)];
 
 let getRoundResult = (pS, computerSelection) => {
-    switch (computerSelection) {
-        case 'rock':
-            return pS === 'rock' ? 't' : pS === 'paper' ? 'p' : 'c'
-        case 'paper':
-            return pS === 'paper' ? 't' : pS === 'scissors' ? 'p' : 'c'
-        case 'scissors':
-            return pS === 'scissors' ? 't' : pS === 'rock' ? 'p' : 'c'
-    }
-}
+  switch (computerSelection) {
+    case "rock":
+      return pS === "rock" ? "t" : pS === "paper" ? "p" : "c";
+    case "paper":
+      return pS === "paper" ? "t" : pS === "scissors" ? "p" : "c";
+    case "scissors":
+      return pS === "scissors" ? "t" : pS === "rock" ? "p" : "c";
+  }
+};
 
-const elComputerLastMove = document.querySelector('#computer-move')
-const elPlayerLastMove = document.querySelector('#player-move')
-const elResult = document.querySelector('#result')
+const elComputerLastMove = document.querySelector("#computer-move");
+const elPlayerLastMove = document.querySelector("#player-move");
+const elResult = document.querySelector("#result");
 
-const elComputerScore = document.querySelector('#computer-score')
-const elPlayerScore = document.querySelector('#player-score')
-const elTieScore = document.querySelector('#tie-score')
+const elComputerScore = document.querySelector("#computer-score");
+const elPlayerScore = document.querySelector("#player-score");
+const elTieScore = document.querySelector("#tie-score");
 
-let computerScore, playerScore, tieScore
-computerScore = playerScore = tieScore = 0
+let computerScore, playerScore, tieScore;
+computerScore = playerScore = tieScore = 0;
 
 let playRound = (playerSelection) => {
-    const computerSelection = getComputerChoice()
-    const res = getRoundResult(playerSelection, computerSelection)
+  const computerSelection = getComputerChoice();
+  const res = getRoundResult(playerSelection, computerSelection);
 
-    let resString
-    if (res === 't') {
-        resString = 'Tie!'
-        elTieScore.textContent = ++tieScore
-    } else if (res === 'p') {
-        resString = 'Player victory!'
-        elPlayerScore.textContent = ++playerScore
-    } else {
-        resString = 'Computer victory!'
-        elComputerScore.textContent = ++computerScore
-    }
+  let resString;
+  if (res === "t") {
+    resString = "Tie!";
+    elTieScore.textContent = ++tieScore;
+  } else if (res === "p") {
+    resString = "Player victory!";
+    elPlayerScore.textContent = ++playerScore;
+  } else {
+    resString = "Computer victory!";
+    elComputerScore.textContent = ++computerScore;
+  }
 
-    elComputerLastMove.textContent = computerSelection
-    elPlayerLastMove.textContent = playerSelection
-    elResult.textContent = resString
+  elComputerLastMove.textContent = computerSelection;
+  elPlayerLastMove.textContent = playerSelection;
+  elResult.textContent = resString;
 
-
-    if (playerScore === 5) {
-        alert(`The player has won with a score of 5-${computerScore}!`)
-        location.reload()
-    } else if (computerScore === 5) {
-        alert(`The computer has won with a score of 5-${playerScore}.`)
-        location.reload()
-    }
-}
-
+  if (playerScore === 5) {
+    alert(`The player has won with a score of 5-${computerScore}!`);
+    location.reload();
+  } else if (computerScore === 5) {
+    alert(`The computer has won with a score of 5-${playerScore}.`);
+    location.reload();
+  }
+};
--- foundations/05-calculator/index.js
+++ foundations/05-calculator/index.js
@@ -1,122 +1,139 @@
-const OPERATIONS = new Set(["+", "-", "×", "÷", "/", "*"])
-const DIV_BY_ZERO_MESSAGE = "Not a number"
+const OPERATIONS = new Set(["+", "-", "×", "÷", "/", "*"]);
+const DIV_BY_ZERO_MESSAGE = "Not a number";
 
 const initialState = {
-    display: "0",
-    storedNum: undefined,
-    operator: undefined,
-    startNewNumber: true,
-}
+  display: "0",
+  storedNum: undefined,
+  operator: undefined,
+  startNewNumber: true,
+};
 
-let state = { ...initialState }
+let state = { ...initialState };
 
-const display = document.querySelector(".display")
+const display = document.querySelector(".display");
 
-
-const handleButtonClick = (event) => handleInput(event.target.textContent)
-const handleKeyPress = (event) => handleInput(event.key)
-
+const handleButtonClick = (event) => handleInput(event.target.textContent);
+const handleKeyPress = (event) => handleInput(event.key);
+//
 // Ugly
 const handleInput = (input) => {
-    if (input === "AC") {
-        state = { ...initialState }
-    } else if (!isNaN(Number(input))) {
-        if (state.startNewNumber) {
-            state.display = input
-            state.startNewNumber = false
-        } else {
-            state.display += input
-        }
-    } else if (OPERATIONS.has(input)) {
-        if (state.storedNum === undefined) {
-            state.storedNum = Number(state.display)
-            state.startNewNumber = true
-        } else if (!state.startNewNumber) {
-            const result = operate(state.storedNum, Number(state.display), state.operator)
-            state.storedNum = result
-            state.display = result
-            state.startNewNumber = true
-        }
-        state.operator = input
-    } else if ((input === "=" || input === "Enter") && state.storedNum && state.operator) {
-        const result = operate(state.storedNum, Number(state.display), state.operator)
-        state.storedNum = result
-        state.display = result
-        state.operator = undefined
-        state.startNewNumber = true
-    } else if (input === "." && !state.display.includes(".")) {
-        if (state.startNewNumber) {
-            state.display = "0."
-            state.startNewNumber = false
-        } else {
-            state.display += "."
-        }
-    } else if (input === "Backspace") {
-
-        if (state.display.length > 1) {
-            state.display = state.display.slice(0, -1)
-        } else {
-            state.display = "0"
-            state.startNewNumber = true
-        }
+  if (input === "AC") {
+    state = { ...initialState };
+  } else if (!isNaN(Number(input))) {
+    if (state.startNewNumber) {
+      state.display = input;
+      state.startNewNumber = false;
+    } else {
+      state.display += input;
     }
+  } else if (OPERATIONS.has(input)) {
+    if (state.storedNum === undefined) {
+      state.storedNum = Number(state.display);
+      state.startNewNumber = true;
+    } else if (!state.startNewNumber) {
+      const result = operate(
+        state.storedNum,
+        Number(state.display),
+        state.operator,
+      );
+      state.storedNum = result;
+      state.display = result;
+      state.startNewNumber = true;
+    }
+    state.operator = input;
+  } else if (
+    (input === "=" || input === "Enter") &&
+    state.storedNum &&
+    state.operator
+  ) {
+    const result = operate(
+      state.storedNum,
+      Number(state.display),
+      state.operator,
+    );
+    state.storedNum = result;
+    state.display = result;
+    state.operator = undefined;
+    state.startNewNumber = true;
+  } else if (input === "." && !state.display.includes(".")) {
+    if (state.startNewNumber) {
+      state.display = "0.";
+      state.startNewNumber = false;
+    } else {
+      state.display += ".";
+    }
+  } else if (input === "Backspace") {
+    if (state.display.length > 1) {
+      state.display = state.display.slice(0, -1);
+    } else {
+      state.display = "0";
+      state.startNewNumber = true;
+    }
+  }
 
-    display.textContent = state.display
-}
+  display.textContent = state.display;
+};
 
 const operate = (num1, num2, operator) => {
-    if (num1 === DIV_BY_ZERO_MESSAGE || num2 === 0) {
-        return DIV_BY_ZERO_MESSAGE
-    }
-    switch (operator) {
-        case "+":
-            return num1 + num2
-        case "-":
-            return num1 - num2
-        case "×":
-        case "*":
-            return num1 * num2
-        case "÷":
-        case "/":
-            return Math.round(((num1 / num2) + Number.EPSILON) * 10000) / 10000
-    }
-}
+  if (num1 === DIV_BY_ZERO_MESSAGE || num2 === 0) {
+    return DIV_BY_ZERO_MESSAGE;
+  }
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "×":
+    case "*":
+      return num1 * num2;
+    case "÷":
+    case "/":
+      return Math.round((num1 / num2 + Number.EPSILON) * 10000) / 10000;
+  }
+};
 
-const getTextWidthEstimate = (size, len) => size * (len / 1.5)
+const getTextWidthEstimate = (size, len) => size * (len / 1.5);
 
 // Over-engineered and yet still impercise
 const updateFontSize = () => {
-    const maxWidth = display.clientWidth
-    const textLength = display.textContent.length
-    const fontSize = Number(window.getComputedStyle(display).fontSize.slice(0, -2))
-    const isOffScreen = getTextWidthEstimate(fontSize, textLength) > maxWidth
+  const maxWidth = display.clientWidth;
+  const textLength = display.textContent.length;
+  const fontSize = Number(
+    window.getComputedStyle(display).fontSize.slice(0, -2),
+  );
+  const isOffScreen = getTextWidthEstimate(fontSize, textLength) > maxWidth;
 
-    if (isOffScreen) {
-        display.style.fontSize = `${fontSize * .9}px`
-    } else {
-        const defaultSize = Math.min(window.innerWidth, window.innerHeight) * .8 / 6;
-        const defaultSizeFits = getTextWidthEstimate(defaultSize, textLength) < maxWidth
-        const increasedFontSize = Math.min(defaultSize, fontSize * 1.1)
-        const incrasedSizeFits = getTextWidthEstimate(increasedFontSize, textLength) < maxWidth
-        const newFontSize = defaultSizeFits ? defaultSize : incrasedSizeFits ? increasedFontSize : fontSize
-        display.style.fontSize = `${newFontSize}px`
-    }
-}
+  if (isOffScreen) {
+    display.style.fontSize = `${fontSize * 0.9}px`;
+  } else {
+    const defaultSize =
+      (Math.min(window.innerWidth, window.innerHeight) * 0.8) / 6;
+    const defaultSizeFits =
+      getTextWidthEstimate(defaultSize, textLength) < maxWidth;
+    const increasedFontSize = Math.min(defaultSize, fontSize * 1.1);
+    const incrasedSizeFits =
+      getTextWidthEstimate(increasedFontSize, textLength) < maxWidth;
+    const newFontSize = defaultSizeFits
+      ? defaultSize
+      : incrasedSizeFits
+        ? increasedFontSize
+        : fontSize;
+    display.style.fontSize = `${newFontSize}px`;
+  }
+};
 
 const showButtonPress = (event) => {
-    const button = event.target
-    button.style.filter = "brightness(150%)"
-    setTimeout(function() {
-        button.style.filter = "brightness(100%)"
-    }, 100);
-}
+  const button = event.target;
+  button.style.filter = "brightness(150%)";
+  setTimeout(function () {
+    button.style.filter = "brightness(100%)";
+  }, 100);
+};
 
 const buttons = document.getElementsByClassName("button");
 for (let i = 0; i < buttons.length; i++) {
-    buttons.item(i).addEventListener("click", handleButtonClick)
-    buttons.item(i).addEventListener("click", updateFontSize)
-    buttons.item(i).addEventListener("click", showButtonPress)
+  buttons.item(i).addEventListener("click", handleButtonClick);
+  buttons.item(i).addEventListener("click", updateFontSize);
+  buttons.item(i).addEventListener("click", showButtonPress);
 }
-document.addEventListener("keydown", handleKeyPress)
-
-
+document.addEventListener("keydown", handleKeyPress);
