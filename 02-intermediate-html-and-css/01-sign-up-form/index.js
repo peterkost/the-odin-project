e242d3d60b4b65568f09d0340c2a3eb71a931f8b
peterkost <46546031+peterkost@users.noreply.github.com>
2024-02-27 12:39:52 -0500

    Complete sign up form

--- 02-intermediate-html-and-css/01-sign-up-form/index.js
+++ 02-intermediate-html-and-css/01-sign-up-form/index.js
@@ -1,1 +1,22 @@
-console.log("linked");
+const passwordFields = [
+  document.getElementById("password"),
+  document.getElementById("passwordConfirm"),
+];
+const passwordMissmatchText = document.getElementById("passwords-missmatch");
+console.log(passwordMissmatchText);
+
+const validatePasswordInput = () => {
+  if (!passwordFields.every((el) => el.value === passwordFields[0].value)) {
+    passwordFields.forEach((el) =>
+      el.setCustomValidity("Passwords do not match!"),
+    );
+    passwordMissmatchText.style.display = "block";
+  } else {
+    passwordFields.forEach((el) => el.setCustomValidity(""));
+    passwordMissmatchText.style.display = "none";
+  }
+};
+
+passwordFields.forEach((el) =>
+  el.addEventListener("input", validatePasswordInput),
+);
