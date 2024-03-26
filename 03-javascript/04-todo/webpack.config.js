3e411cca353a53ab71487dbdd269ea38342e0101
peterkost <46546031+peterkost@users.noreply.github.com>
2024-03-26 10:51:09 -0400

    Add sourcemap to webpack

--- 03-javascript/04-todo/webpack.config.js
+++ 03-javascript/04-todo/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -24,4 +25,5 @@ module.exports = {
     ],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  devtool: "cheap-source-map",
 };
