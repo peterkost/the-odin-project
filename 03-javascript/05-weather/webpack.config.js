0c44be553f1fe88303611d8f3ea455c32e4dec50
peterkost <46546031+peterkost@users.noreply.github.com>
2024-03-30 12:17:12 -0400

    Initial weather commit

---
+++ 03-javascript/05-weather/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "cheap-source-map",
+  devServer: {
+    static: "./dist",
+    open: {
+      app: {
+        name: "Google Chrome",
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
