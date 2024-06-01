3d29f35fee483622538f1e9136cd303aa1a719c0
peterkost <46546031+peterkost@users.noreply.github.com>
2024-05-31 20:11:54 -0400

    feat(battleship): place ships of correct sizes randomly

--- 03-javascript/12-battleship/src/logic/player.js
+++ 03-javascript/12-battleship/src/logic/player.js
@@ -2,17 +2,15 @@ import GameBoard from "./gameboard";
 import { Coords } from "./types";
 
 export default class Player {
-  #attacked = new Set();
-
   #unAttacked = Array.from({ length: 10 }, (_, x) =>
     Array.from({ length: 10 }, (_, y) => [x, y]),
   ).flat();
 
-  constructor(isComputer, mockShips = false) {
+  constructor(isComputer = false, mockShips = false) {
     this.isComputer = isComputer;
     this.gameBoard = new GameBoard();
     if (mockShips) {
-      this.#generateBoard();
+      this.randomlyPlaceShips();
     }
   }
 
@@ -30,5 +28,29 @@ export default class Player {
     const randomIndex = Math.floor(Math.random() * this.#unAttacked.length);
     const coords = this.#unAttacked.splice(randomIndex, 1)[0];
     return Coords(coords[0], coords[1]);
+  }
+
+  randomlyPlaceShips() {
+    const sizes = [2, 3, 3, 4, 5];
+
+    sizes.forEach((size) => {
+      let placed = this.gameBoard.place(...this.#getCoordPairOfSize(size));
+      while (!placed) {
+        placed = this.gameBoard.place(...this.#getCoordPairOfSize(size));
+      }
+    });
+  }
+
+  #getCoordPairOfSize(size) {
+    const horizontal = Math.random() > 0.5;
+
+    const a = Math.floor(Math.random() * (10 - size));
+    const b = Math.floor(Math.random() * 10);
+
+    if (horizontal) {
+      return [Coords(b, a), Coords(b, a + size - 1)];
+    } else {
+      return [Coords(a, b), Coords(a + size - 1, b)];
+    }
   }
 }
--- 03-javascript/12-battleship/src/logic/player.test.js
+++ 03-javascript/12-battleship/src/logic/player.test.js
@@ -1,48 +1,64 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, test, expect, beforeEach } from "@jest/globals";
 import Player from "./player";
 import GameBoard from "./gameboard";
 
 describe("Player", () => {
+  let player;
+  beforeEach(() => (player = new Player()));
+
   test("Contains Game Board", () => {
-    const player = new Player(false);
     expect(player.gameBoard).toBeInstanceOf(GameBoard);
   });
 
   test("Initializes as computer", () => {
-    const player = new Player(true);
+    player = new Player(true);
     expect(player.isComputer).toBe(true);
   });
 
   test("Initializes as person", () => {
-    const player = new Player(false);
     expect(player.isComputer).toBe(false);
   });
 
   test("Initialize with empty board", () => {
-    const player = new Player(false);
     expect(player.gameBoard.areAllSunk()).toBe(true);
-  });
-
-  test("Initialize with mock ships", () => {
-    const player = new Player(false, true);
-    expect(player.gameBoard.areAllSunk()).toBe(false);
   });
 
   describe("NPC Attack", () => {
     test("Returns a coordinate", () => {
-      const player = new Player(false, true);
       expect(player.getAttack()).toHaveProperty("x");
       expect(player.getAttack()).toHaveProperty("y");
     });
 
     test("Coordinate is on unexplored tile", () => {
-      const player = new Player(false, true);
       const attacked = new Set();
       for (let i = 0; i < 100; i++) {
         const attack = player.getAttack();
         expect(attacked).not.toContain(attack.hash());
         attacked.add(attack.hash());
       }
+    });
+  });
+
+  describe("NPC Ship placement", () => {
+    beforeEach(() => player.randomlyPlaceShips());
+
+    test("Places five ships", () => {
+      expect(player.gameBoard.ships.size).toBe(5);
+    });
+
+    test("Ships are of required lengths", () => {
+      const expected = [0, 0, 1, 2, 1, 1];
+      const actual = [0, 0, 0, 0, 0, 0];
+      player.gameBoard.ships.forEach((v) => actual[v.length]++);
+
+      expect(expected).toEqual(actual);
+    });
+
+    test("Ships are deployed randomly", () => {
+      const secondPlayer = new Player();
+      secondPlayer.randomlyPlaceShips();
+
+      expect(player.gameBoard.board).not.toEqual(secondPlayer.gameBoard.board);
     });
   });
 });
