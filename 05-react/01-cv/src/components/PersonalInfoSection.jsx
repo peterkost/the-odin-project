9b5cc7d79d722a7c3d9bddeb6c47e85ef67ad7c7
peterkost <46546031+peterkost@users.noreply.github.com>
2024-06-07 21:44:44 -0400

    refactor(cv): remove unnecessary state lift

--- 05-react/01-cv/src/components/PersonalInfoSection.jsx
+++ 05-react/01-cv/src/components/PersonalInfoSection.jsx
@@ -1,47 +1,15 @@
-import { useState } from "react";
 import SectionHeading from "./SectionHeading";
 import TextField from "./TextField";
 
 const PeronalInfoSection = ({ editing }) => {
-  const [first, setFirst] = useState("");
-  const handleFirstChange = (value) => setFirst(value);
-  const [last, setLast] = useState("");
-  const handleLastChange = (value) => setLast(value);
-  const [email, setEmail] = useState("");
-  const handleEmailChange = (value) => setEmail(value);
-  const [phone, setPhone] = useState("");
-  const handlePhoneChange = (value) => setPhone(value);
-
   return (
     <>
       <SectionHeading title="Personal Information" />
       <div className="two-col">
-        <TextField
-          label="First Name"
-          value={first}
-          onChange={handleFirstChange}
-          disabled={!editing}
-        />
-        <TextField
-          label="Last Name"
-          value={last}
-          onChange={handleLastChange}
-          disabled={!editing}
-        />
-        <TextField
-          label="Email"
-          value={email}
-          type="email"
-          onChange={handleEmailChange}
-          disabled={!editing}
-        />
-        <TextField
-          label="Phone Number"
-          value={phone}
-          type="phone"
-          onChange={handlePhoneChange}
-          disabled={!editing}
-        />
+        <TextField label="First Name" disabled={!editing} />
+        <TextField label="Last Name" disabled={!editing} />
+        <TextField label="Email" type="email" disabled={!editing} />
+        <TextField label="Phone Number" type="phone" disabled={!editing} />
       </div>
     </>
   );
--- 05-react/01-cv/src/components/TextField.jsx
+++ 05-react/01-cv/src/components/TextField.jsx
@@ -1,28 +1,13 @@
 import "../styles/TextField.css";
 
-const TextField = ({ type = "text", label, onChange, value, ...props }) => {
-  const handleChange = (event) => onChange(event.target.value);
-
+const TextField = ({ type = "text", label, ...props }) => {
   return (
     <div className="input-container">
       <label htmlFor={label}>{label}</label>
       {type === "textarea" ? (
-        <textarea
-          name={label}
-          id={label}
-          value={value}
-          onChange={handleChange}
-          {...props}
-        />
+        <textarea name={label} id={label} {...props} />
       ) : (
-        <input
-          type={type}
-          name={label}
-          id={label}
-          value={value}
-          onChange={handleChange}
-          {...props}
-        />
+        <input type={type} name={label} id={label} {...props} />
       )}
     </div>
   );
