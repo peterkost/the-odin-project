1e9f14fcf3c43ba8a1e7d795af0b859c388ef118
peterkost <46546031+peterkost@users.noreply.github.com>
2024-06-09 15:25:10 -0400

    feat(memory): style painting border

--- 05-react/02-memory/src/components/Gameboard/components/Card/Card.jsx
+++ 05-react/02-memory/src/components/Gameboard/components/Card/Card.jsx
@@ -2,9 +2,7 @@ import "./Card.css";
 const Card = ({ card, onClick }) => {
   return (
     <div className="card-frame" onClick={() => onClick(card.id)}>
-      <div className="inner-frame">
-        <img className="card-image" src={card.image} alt={card.name} />
-      </div>
+      <img className="card-image" src={card.image} alt={card.name} />
     </div>
   );
 };
