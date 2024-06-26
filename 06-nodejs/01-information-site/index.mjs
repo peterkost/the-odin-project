0e4a0cb4535d3aac479a87578341bafc182dc3da
peterkost <46546031+peterkost@users.noreply.github.com>
2024-06-26 16:31:15 -0400

    feat(basic-info): serve html pages

--- 06-nodejs/01-information-site/index.mjs
+++ 06-nodejs/01-information-site/index.mjs
@@ -1,26 +1,32 @@
 import { createServer } from "http";
 import { URL } from "url";
+import fs from "node:fs";
 
 const INDEX_ROUTE = "/";
 const ABOUT_ROUTE = "/about";
 const CONTACT_ROUTE = "/contact-me";
 
+const INDEX_HTML = fs.readFileSync("./pages/index.html");
+const ABOUT_HTML = fs.readFileSync("./pages/about.html");
+const CONTACT_HTML = fs.readFileSync("./pages/contact-me.html");
+const ERROR_HTML = fs.readFileSync("./pages/404.html");
+
 createServer((req, res) => {
   const reqUrl = new URL(req.url, `http://${req.headers.host}`).pathname;
 
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
   switch (reqUrl) {
     case INDEX_ROUTE:
-      res.end("INDEX");
-      return;
+      res.end(INDEX_HTML);
+      break;
     case ABOUT_ROUTE:
-      res.end("ABOUT");
-      return;
+      res.end(ABOUT_HTML);
+      break;
     case CONTACT_ROUTE:
-      res.end("CONTACT");
-      return;
+      res.end(CONTACT_HTML);
+      break;
     default:
-      res.end("ERROR");
+      res.end(ERROR_HTML);
   }
 }).listen(8080);
