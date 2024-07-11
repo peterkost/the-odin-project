74bf14c3269d97d890f8f4c8dd7b1c3c1a3d51c9
peterkost <46546031+peterkost@users.noreply.github.com>
2024-07-11 17:50:48 -0400

    feat(message-board): post new messages

--- 06-nodejs/02-message-board/routes/index.js
+++ 06-nodejs/02-message-board/routes/index.js
@@ -14,9 +14,21 @@ const messages = [
   },
 ];
 
-/* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
